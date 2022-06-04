0136682cedbe29c29b6299b8204ac6b33a0de9bb
Anne Ströhlein <anruka@gmail.com>
2022-06-04 14:50:14 +0200

    Adding all Crud-Actions & routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :articles, only: [:show,:index]
+  resources :articles
 end
