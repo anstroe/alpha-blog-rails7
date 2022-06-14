6d6fc4b0310f71b36755fc8c60ffe908b9e0aed4
Anne Ströhlein <anruka@gmail.com>
2022-06-14 10:27:48 +0200

    Added login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "pages#home"
   get "about", to: "pages#about"
-  # Defines the root path route ("/")
-  # root "articles#index"
   resources :articles
   get "signup", to: "users#new"
   resources :users, except: [ :new ]
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 end
