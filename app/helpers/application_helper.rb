f3374e963b210875796a0826f6f4ff07ee2bcdb0
Anne Ströhlein <anruka@gmail.com>
2022-06-14 11:37:04 +0200

    Added restrictions for users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,11 +8,4 @@ module ApplicationHelper
     image_tag(gravator_url, alt: user.username, class: "rounded mx-auto d-block shadow")
   end
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]      
-  end
-
-  def logged_in?
-    !!current_user
-  end
 end
